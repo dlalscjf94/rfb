ba843c07b8ff94f6623447f14163bedf28915b80
dlalscjf94 <ssw01092@naver.com>
2022-06-19 18:32:16 +0900

    add css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0); 
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("call an api"); 
   return (
     <div className="App">
-      <h1>Welcome back!</h1>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      <Button text = "Continue" />
     </div>
   );
 }
